fdbc9f28309837730f8122a3159819dae5bb7ece
Kamil Gusowski <47417538+gkns1@users.noreply.github.com>
2021-03-22 12:28:52 -0400

    Update interest.py

--- interest.py
+++ interest.py
@@ -1,5 +1,3 @@
-# Simple script to calculate how many times and in-game bank interest has been accumulated. It accumulates every 48 minutes a character has been logged in. If someone logs out before the 48 minutes is up, the timer restarts and interest is lost. Logging has to be turned on in the game options for the script to work.
-# The logs are not utf-8 encoded, so regional characters are broken. It does not affect this script
 from datetime import datetime
 import re
 
